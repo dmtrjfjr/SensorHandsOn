ece819109c547498bbd9473ae483affa8e66b609
Dimitrij <dimitrijfajar@yahoo.co.uk>
2019-07-11 11:24:57 +0700

    Initial Commit

---
+++ SensorHandsOn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SensorHandsOn
+//
+//  Created by Dimitrij Fajar Satria Dharma on 11/07/19.
+//  Copyright © 2019 Dimitrij Fajar Satria Dharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
