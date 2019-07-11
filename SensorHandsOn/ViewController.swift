00102572b62f87779b1b981432517143a488eb1d
Dimitrij <dimitrijfajar@yahoo.co.uk>
2019-07-11 11:57:20 +0700

    initial commit

--- SensorHandsOn/ViewController.swift
+++ SensorHandsOn/ViewController.swift
@@ -7,14 +7,84 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    let motion = CMMotionManager()
+    let altimeter = CMAltimeter()
+    let pedometer = CMPedometer()
+    let activityManager = CMMotionActivityManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//        startMotion()
+//        startAlti()
+        startPedo()
+        startAM()
     }
-
-
+    
+    func startAM(){
+        if CMMotionActivityManager.isActivityAvailable(){
+            activityManager.startActivityUpdates(to: OperationQueue.current!) { (dataM) in
+                if let data = dataM{
+                    print(data)
+                }
+            }
+        }
+    }
+    
+    func startPedo(){
+        if CMPedometer.isPedometerEventTrackingAvailable(){
+            pedometer.startUpdates(from: Date()) { (pedoData, Error) in
+                if let data = pedoData{
+                    print(data.numberOfSteps)
+                }
+            }
+        }
+    }
+    
+//    func startAlti(){
+//        if CMAltimeter.isRelativeAltitudeAvailable(){
+//            altimeter.startRelativeAltitudeUpdates(to: OperationQueue.current!) { (altiData, Error) in
+//                if let data = altiData{
+//                     print(data)
+//                }
+//            }
+//        }
+//    }
+    
+//    func startMotion(){
+//        //detect gyro nya avail atau engga
+//        if motion.isGyroAvailable{
+//            motion.startGyroUpdates(to: OperationQueue.current!) { (GyroData, Error) in
+//                if let data =  GyroData{
+//                    if data.rotationRate.x >= 1 {
+//                        print("up")
+//                    }
+//                }
+//            }
+//        }
+//
+//        if motion.isAccelerometerAvailable{
+//            motion.startAccelerometerUpdates(to: OperationQueue.current!) { (accelData, Error) in
+//                if let data = accelData{
+//                    if data.acceleration.y >= 0.3 {
+//                        print("tergoyang")
+//                    }
+//                }
+//            }
+//        }
+//
+//        if motion.isMagnetometerAvailable{
+//            motion.startMagnetometerUpdates(to: OperationQueue.current!) { (magnetoData, Error) in
+//                if let data = magnetoData{
+//                    print(data)
+//                }
+//            }
+//        }
+//    }
+    
 }
 
